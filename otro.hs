fa19053ab939b1c3562251a8ca75375ba42d185a
Totoferreyra <totoferrey@gmail.com>
2024-06-12 17:14:11 -0300

    cambio multiplicacion

---
+++ otro.hs
@@ -0,0 +1,8 @@
+suma :: Int -> Int -> Int
+suma n1 n2 = n1 + n2
+
+resta :: Int -> Int -> Int
+resta n1 n2 = n1 - n2
+
+multiplicacion :: Int -> Int -> Int -> Int
+multiplicacion n1 n2 n3 = n1 * n2 * n3
